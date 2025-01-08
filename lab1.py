48704296edcc84ba51c4ebe5f8b04951b47f8faa
JeTsEt627 <sasha-kanev6@mail.ru>
2025-01-08 18:56:50 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,7 @@
+a = int(input("Введите ваше число: "))
+for i in range(1, a+1):
+    print(i)
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+print("Большее число:", max(a, b))
